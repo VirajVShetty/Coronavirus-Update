97a2a041fd6804faa60da4db1e7e0fd39496fa78
Viraj Shetty <viraj713@gmail.com>
2020-06-16 13:55:18 +0530

    updated file

--- Corona_Updates.py
+++ Corona_Updates.py
@@ -1,4 +1,11 @@
-""" Authors - Akshay Sonawane and Viraj Shetty """
+"""
+    Authors:
+
+    Viraj Shetty
+    Akshay Sonawane
+    
+"""
+import urllib
 import urllib.request, urllib.parse
 from bs4 import BeautifulSoup
 import ssl
@@ -24,9 +31,35 @@ for tag in tags:
     if x.isnumeric():
         if x not in stats:
             stats.append(x)
+            
 stat = stats[:4]
 print("Corona Updates")
+print("---------------------")
+print("India")
+print(" ")
 print("Active: "+stat[0])
 print("Cured: "+stat[1])
 print("Deaths: "+stat[2])
 print("Migrated: "+stat[3])
+print("---------------------")
+
+state = []
+x = soup.select('.table > tbody:nth-child(2) > tr:nth-child(20) > td:nth-child(3)')
+y = re.findall(r'\d+',str(x[0]))
+state.append(int(y[0]))
+x = soup.select('.table > tbody:nth-child(2) > tr:nth-child(20) > td:nth-child(4)')
+y = re.findall(r'\d+',str(x[0]))
+state.append(int(y[0]))
+x = soup.select('.table > tbody:nth-child(2) > tr:nth-child(20) > td:nth-child(5)')
+y = re.findall(r'\d+',str(x[0]))
+state.append(int(y[0]))
+x = soup.select('.table > tbody:nth-child(2) > tr:nth-child(20) > td:nth-child(6)')
+y = re.findall(r'\d+',str(x[0]))
+state.append(int(y[0]))
+print("---------------------")
+print("Maharashtra")
+print(" ")
+print("Active: "+str(state[0]))
+print("Cured: "+str(state[1]))
+print("Deaths: "+str(state[2]))
+print("Total: "+str(state[3]))
