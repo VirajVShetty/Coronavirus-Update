8fe040fc649773a1d880f4f186e20d45cb3a62e5
akshaysonawaneS <akshaysonawane10526@gmail.com>
2020-06-16 18:34:19 +0530

    changed update fun

--- EmailSender.py
+++ EmailSender.py
@@ -2,30 +2,37 @@
 """ Authors - Akshay Sonawane and Viraj Shetty """
 
 import smtplib
+from Corona_Updates import update
 
 sender = 'corona.updates.av.2020@gmail.com'
 receivers = ['akshaysonawane10526@gmail.com', 'virajshetty1@hotmail.com']
 pwd = 'sakecboys-akvi'
 
 
-message = """From: From Your Daily Coronavirus Updater <from@fromdomain.com>
-Subject: Coronavirus Cases Update
-
-Current Active Cases     
-    India :- {}             
-    Maharasgtra :- {}       
-    Mumbai :- {}   
-
-People Recovered:
-    India :- {}             
-    Maharasgtra :- {}       
-    Mumbai :- {} 
-
-Total Recovery Percentage: {}%
-
-""".format("6969","654","Will update soon","6969","654","63738", "Will update soon")
-
 if __name__ == "__main__":
+
+    india_list, india_percent, maha_list, maha_percent = update()
+
+    message = """\
+    From: From Person <from@fromdomain.com>
+    To: To Person <to@todomain.com>
+    Subject: Coronavirus Cases Update
+
+    Current Active Cases     
+        India :- {}             
+        Maharasgtra :- {}       
+        Mumbai :- {}   
+
+    People Recovered:
+        India :- {}             
+        Maharasgtra :- {}       
+        Mumbai :- {} 
+
+    Death in india: {}
+    Total Recovery Percentage: {}%
+
+    """.format(india_list[0], maha_list[0], "work in progress", india_list[1], maha_list[1], "Work in progress",india_list[2], india_percent)
+
     try:
         smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
         smtpObj.starttls()
