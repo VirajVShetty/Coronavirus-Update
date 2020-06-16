8bb672e97328347fae077954f664467b637b6f27
akshaysonawaneS <akshaysonawane10526@gmail.com>
2020-06-16 13:28:14 +0530

    basic email sender

--- EmailSender.py
+++ EmailSender.py
@@ -5,21 +5,21 @@ import smtplib
 
 sender = 'corona.updates.av.2020@gmail.com'
 receivers = ['akshay.sonawane@sakec.ac.in']
+pwd = 'sakecboys-akvi'
 
 message = """From: From Your Daily Coronavirus Updater <from@fromdomain.com>
 Subject: Coronavirus Cases Update
 
-This is a test e-mail message.
+This is a test e-mail message. yooo
 """
 
 if __name__ == "__main__":
     try:
         smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
         smtpObj.starttls()
-        smtpObj.login('corona.updates.av.2020', 'sakecboys-akvi')
+        smtpObj.login(sender, pwd)
         smtpObj.sendmail(sender, receivers, message)
-        print
-        "Successfully sent email"
+        print("Successfully sent email")
     except smtplib.SMTPException:
         print
         "Error: unable to send email"
